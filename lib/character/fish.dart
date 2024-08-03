10374ae2ebb2524c3ed207160c8ec9b13833a781
TungNV <tung.nguyenvuong@embedit.com>
2024-08-03 12:40:08 +0700

    fish moving

--- lib/character/fish.dart
+++ lib/character/fish.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math' as math;
 
 import 'package:carium/acarium_flame_game.dart';
 import 'package:flame/components.dart';
@@ -9,8 +10,12 @@ class Fish extends SpriteGroupComponent with HasGameRef<Acarium> {
   double accumulateTime = 0;
   double fixedDeltaTime = 1 / 60;
   Vector2 velocity = Vector2.zero();
+  double direction = 0.0;
   double horizontalMovement = 1;
   double moveSpeed = 100;
+
+  Fish({required position, required this.direction})
+      : super(position: position);
 
   @override
   FutureOr<void> onLoad() {
@@ -20,6 +25,10 @@ class Fish extends SpriteGroupComponent with HasGameRef<Acarium> {
       FishType.fish1: sprite,
     };
     current = FishType.fish1;
+    if (direction >= 90 && direction <= 270) {
+      flipVertically();
+    }
+    add(TextComponent(text: direction.toString(), scale: Vector2(2, 2)));
     return super.onLoad();
   }
 
@@ -30,11 +39,19 @@ class Fish extends SpriteGroupComponent with HasGameRef<Acarium> {
       _onMove(dt);
       accumulateTime -= fixedDeltaTime;
     }
+
     super.update(dt);
   }
 
   _onMove(double dt) {
-    velocity.x = horizontalMovement * moveSpeed;
-    position.x += velocity.x * dt;
+    transform.angleDegrees = direction;
+
+    velocity.x = ((moveSpeed) * (math.cos(transform.angle)));
+    velocity.y = ((moveSpeed) * (math.sin(transform.angle)));
+    // velocity.y =  moveSpeed * direction.y;
+    position.x -= velocity.x * dt;
+    position.y -= velocity.y * dt;
+    // position.y += velocity.y * dt * transform.angle;
+    // position.y += velocity.y * dt;
   }
 }
--- lib/level/tank.dart
+++ lib/level/tank.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math' as math;
 
 import 'package:carium/character/fish.dart';
 import 'package:flame/components.dart';
@@ -9,9 +10,25 @@ class Tank extends World {
   @override
   FutureOr<void> onLoad() async {
     level = await TiledComponent.load('ocean.tmx', Vector2.all(32));
+    // level = PositionComponent();
     add(level);
     // final spawnFish =
-    add(Fish());
+    // add(Fish());
+    _generateFish();
     return super.onLoad();
+  }
+
+  _generateFish() {
+    var rnd = math.Random();
+    for (var i = 0; i < 10; i++) {
+      final fish = Fish(
+        // fruit: spawnPoint.name,
+        position: Vector2(rnd.nextDouble() * 3000, rnd.nextDouble() * 2000),
+        direction: rnd.nextDouble() * 360,
+        // direction: 180,
+        // size: Vector2(spawnPoint.width, spawnPoint.height),
+      );
+      add(fish);
+    }
   }
 }
